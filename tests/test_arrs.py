cb5f4dcd864caec616980a2936678d7bb1a2cbdb
Artem <akirillov@itps-russia.ru>
2023-02-21 18:21:26 +0500

    Создание тестов по unittest

---
+++ tests/test_arrs.py
@@ -0,0 +1,27 @@
+import unittest
+from utils.arrs import get, my_slice
+
+class TestGet(unittest.TestCase):
+
+    def test_positive_index(self):
+        self.assertEqual(get([1, 2, 3],1), 2)
+
+    def test_negative_index(self):
+        self.assertEqual(get([1, 2, 3],-1), None)
+
+class TestMy_slice(unittest.TestCase):
+
+    def test_positive_start(self):
+        self.assertEqual(my_slice([1, 2, 3],1), [2, 3])
+        self.assertEqual(my_slice([1, 2, 3], 1, 3), [2, 3])
+
+    def test_negative_start(self):
+        self.assertEqual(my_slice([1, 2, 3], -1), [3])
+        self.assertEqual(my_slice([1, 2, 3], -4), [1, 2, 3])
+
+    def test_empty_array(self):
+        self.assertEqual(my_slice([], 0), [])
+
+
+#if __name__ == '__main__':
+#    unittest.main()
